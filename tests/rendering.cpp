d07b5e48f8b4d07c3c61a4d3a9924ffd05d479c0
lzubiaur <zubiaur.laurent@gmail.com>
2017-11-30 10:58:09 +0100

    Test simple SDL window creation

--- tests/rendering.cpp
+++ tests/rendering.cpp
@@ -1,10 +1,44 @@
 #include "catch.hpp"
 #include <iostream>
-#include "SDL.h"
+#include <SDL.h>
 
-TEST_CASE("Simple SDL init test", "[RENDERING]") {
-    SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO);
+TEST_CASE("Simple SDL window test", "[RENDERING]") {
+    SDL_SetMainReady();
+    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO) != 0) {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        SDL_Quit();
+        return;
+    }
 
-    SDL_Quit()
-    REQUIRE(false);
+    SDL_Window *window = SDL_CreateWindow (
+        "An SDL2 window",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        640, 480,
+        SDL_WINDOW_SHOWN
+    );
+
+    if (window == NULL) {
+        // In the case that the window could not be made...
+        printf("Could not create window: %s\n", SDL_GetError());
+        SDL_Quit();
+        return;
+    }
+
+    bool quit = false;
+    SDL_Event e;
+    while (quit == false) {
+        while (SDL_PollEvent(&e)){
+            if (e.type == SDL_QUIT){
+                quit = true;
+            }
+            if (e.type == SDL_KEYDOWN){
+                quit = true;
+            }
+            if (e.type == SDL_MOUSEBUTTONDOWN){
+                quit = true;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
 }
